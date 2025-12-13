042b546602e872c21b0d58a8065bfc1b63179c08
yaobo <15170970773@163.com>
2025-12-13 15:37:04 +0800

    fix(initOrm): 启用数据库同步以支持开发环境
    在生产环境中应保持为false，开发时设为true便于自动同步实体变更

--- src/utils/initOrm.ts
+++ src/utils/initOrm.ts
@@ -15,7 +15,7 @@ export function initOrm() {
     password: process.env.DATABASE_PASSWORD ?? 'yaobo20030902',
     database: process.env.DATABASE_NAME ?? 'jbook',
     entities: [Post, Category, Reply, Tag, Comment, File],
-    synchronize: false,
+    synchronize: true,
     logging: ['error'],
   });
 }
